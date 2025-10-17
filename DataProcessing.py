97b12e1c0d7f970b089d1cd35a2390499b191a44
Bernardo Ameneyro <bameneyr@vols.utk.edu>
2025-10-16 21:45:50 -0400

    Changed map to only US and cleaned up a bit

--- DataProcessing.py
+++ DataProcessing.py
@@ -1,5 +1,8 @@
 def fix_name(name: str) -> str:
-    return "Year " + name.split(maxsplit=1)[0]
+    return name.split(maxsplit=1)[0] + " Opioid Related Mortality Rate"
 
 def get_column_names(column_names: list[str]) -> list[str]:
     return [name.lower() if name=="FIPS" else fix_name(name) for name in column_names]
+
+def int_to_column_name(year: int) -> str:
+    return f"{year} Opioid Related Mortality Rate"
--- main.py
+++ main.py
@@ -1,43 +1,43 @@
+import json
+from urllib.request import urlopen
 import polars as pl
 import plotly.express as px
-import json
 from dash import Dash, html, dcc, callback, Output, Input
-from urllib.request import urlopen
-from DataProcessing import get_column_names
+from DataProcessing import get_column_names, int_to_column_name
 
 with urlopen("https://raw.githubusercontent.com/plotly/datasets/master/geojson-counties-fips.json") as response:
     counties = json.load(response)
 
-mortality_rates_df = pl.scan_csv("Data/mortality_rates.csv",
-                                 with_column_names=get_column_names)
-
-years_dict = {index : year for index, year in enumerate(mortality_rates_df.collect_schema().names()[1:])}
+mortality_rates_df: pl.LazyFrame = pl.scan_csv("Data/mortality_rates.csv", with_column_names=get_column_names)
 
 app = Dash(__name__)
-
 app.layout = [
     html.H1(children="Opioid Related Mortality in the US", style={"textAlign": "center"}),
-    dcc.Slider(marks=years_dict, value=1, id="slider-selection"),
-    dcc.Graph(id="mortality-graph", style={"height": 1000})
+    html.Figure([
+        html.P("Select a year:"),
+        dcc.Slider(min=2010, max=2022, step=1, value=2017, marks={i: f"{i}" for i in range(2010,2023)}, id="select-year"),
+        dcc.Graph(id="mortality-graph", style={"height": 1000})
+    ])
 ]
 
 @callback(
     Output("mortality-graph", "figure"),
-    Input("slider-selection", "value")
+    Input("select-year", "value")
 )
 def update_mortality_graph(year: int):
-    yearly_mortality_rates_df = mortality_rates_df.select(["fips", years_dict[year]])
-    return px.choropleth_map(yearly_mortality_rates_df.collect(), geojson=counties, locations="fips", color=years_dict[year],
-                            color_continuous_scale="bluered",
-                            map_style="carto-positron",
-                            zoom=3, center = {"lat": 37.0902, "lon": -95.7129},
-                            opacity=0.5,
-                            labels={years_dict[year]:"mortality rate"})
+    year_column: str = int_to_column_name(year)
+    yearly_mortality_rates_df: pl.LazyFrame = mortality_rates_df.select(["fips", year_column])
 
+    return px.choropleth(yearly_mortality_rates_df.collect(),
+                         geojson=counties,
+                         locations="fips",
+                         color=year_column,
+                         scope="usa",
+                         color_continuous_scale="bluered",
+                         labels={year_column:"mortality rate"}
+                         )
 
-def main() -> None:
-    app.run(debug=True)
 
 
 if __name__ == "__main__":
-    main()
+    app.run(debug=True)
