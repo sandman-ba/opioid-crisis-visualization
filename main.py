063cf024940bc323c6ea468f622c204ab1ec6ca9
Bernardo Ameneyro <bameneyr@vols.utk.edu>
2025-10-22 11:22:25 -0400

    Merged Interstates and Mortality Maps
    - Added Patches to only update necessary data on callbacks
    
    - Adde button to toggle interstates

--- main.py
+++ main.py
@@ -3,7 +3,7 @@ import polars as pl
 import plotly.express as px
 import plotly.graph_objects as go
 from typing import Any
-from dash import Dash, html, dcc, callback, Output, Input
+from dash import Dash, html, dcc, callback, Output, Input, ctx, Patch
 from DataProcessing import get_column_names, int_to_column_name
 
 with open("Data/cb_2022_us_county_20m.geojson") as counties_file:
@@ -26,48 +26,72 @@ interstates: pl.LazyFrame = pl.LazyFrame({
     "lon": interstate_lons
 })
 
-
 mortality_rates_df: pl.LazyFrame = pl.scan_csv("Data/mortality_rates.csv", with_column_names=get_column_names)
-
-mortality_figure: go.Figure = px.line_geo(
-    interstates.collect(),
-    lon = "lon",
-    lat = "lat",
-    hover_name = "name",
-    scope = "usa"
+mortality_figure: go.Figure = go.Figure()
+mortality_figure.add_trace(
+    go.Choropleth(
+        geojson = counties,
+        featureidkey = "properties.GEOID",
+        locations = mortality_rates_df.select("fips").collect()["fips"],
+        z = mortality_rates_df.select(int_to_column_name(2017)).collect()[int_to_column_name(2017)],
+        colorscale = "bluered",
+        colorbar_title = "Mortality Rate",
+        name = "Mortality"
+    )
 )
+mortality_figure.add_trace(
+    go.Scattergeo(
+        lon = interstates.select("lon").collect()["lon"],
+        lat = interstates.select("lat").collect()["lat"],
+        hovertext = interstates.select("name").collect()["name"],
+        mode = "lines",
+        line = {"color": "black", "width": 2},
+        name = "Interstates",
+        visible = False
+    )
+)
+mortality_figure.update_layout(geo_scope = "usa")
+
+def update_mortality(year: int) -> Patch:
+    year_column: str = int_to_column_name(year)
+    mortality_patch = Patch()
+    mortality_patch.data[0]["z"] = list(mortality_rates_df.select(year_column).collect()[year_column])
+    return mortality_patch
+
+def update_interstates(button_state: int) -> go.Figure:
+    interstates_patch = Patch()
+    if button_state % 2 != 0:
+        interstates_patch.data[1]["visible"] = True
+    else:
+        interstates_patch.data[1]["visible"] = False
+    return interstates_patch
+
 
 app = Dash(__name__)
 app.layout = [
     html.H1(children="Opioid Related Mortality in the US", style={"textAlign": "center"}),
-    html.Figure([
-        dcc.Graph(id="roads-graph", style={"height": 1000}, figure=mortality_figure)
+    html.Aside([
+        html.Button("Toggle Interstates", id="toggle-interstates", n_clicks=0)
     ]),
     html.Figure([
         html.P("Select a year:"),
         dcc.Slider(min=2010, max=2022, step=1, value=2017, marks={i: f"{i}" for i in range(2010,2023)}, id="select-year"),
-        dcc.Graph(id="mortality-graph", style={"height": 1000})
+        dcc.Graph(id="mortality-graph", style={"height": 1000}, figure=mortality_figure)
     ])
 ]
 
 @callback(
     Output("mortality-graph", "figure"),
-    Input("select-year", "value")
+    Input("select-year", "value"),
+    Input("toggle-interstates", "n_clicks"),
+    prevent_initial_call = True
 )
-def update_mortality_graph(year: int):
-    year_column: str = int_to_column_name(year)
-    yearly_mortality_rates_df: pl.LazyFrame = mortality_rates_df.select(["fips", year_column])
-
-    return px.choropleth(yearly_mortality_rates_df.collect(),
-                         geojson=counties,
-                         featureidkey="properties.GEOID",
-                         locations="fips",
-                         color=year_column,
-                         scope="usa",
-                         color_continuous_scale="bluered",
-                         labels={year_column:"mortality rate"}
-                         )
-
+def update_mortality_graph(year: int, button_state: int) -> go.Figure:
+    trigger_id = ctx.triggered_id
+    if trigger_id == "select-year":
+        return update_mortality(year)
+    elif trigger_id == "toggle-interstates":
+        return update_interstates(button_state)
 
 
 if __name__ == "__main__":
